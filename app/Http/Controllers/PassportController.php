0cbb996752d2bfb3a6c9c58d9e52fd2ac83b907f
Diouani <73253036+Diouani@users.noreply.github.com>
2021-08-06 19:02:20 +0100

    Update PassportController.php

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -47,7 +47,7 @@ class PassportController extends Controller
                 $token = auth()->user()->createToken('S-login')->accessToken;
                 return response()->json(['token' => $token], 200);
             } else {
-                return response()->json(['error' => 'UnAuthorised'], 401);
+                return response()->json(['error' => 'UnAuthorised','status' => '401']);
             }
         }
 
