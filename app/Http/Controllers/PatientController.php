c0b55eeeda005895cc3875650ac3fa45be87660c
Diouani <73253036+Diouani@users.noreply.github.com>
2021-08-12 14:42:51 +0100

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -56,7 +56,7 @@ class PatientController extends Controller
 
     }else{
      // User::find()
-
+     $user_id = auth()->user()->id;
      Patient::create([
         'user_id' => $user_id,
         'first_name'=> $request->first_name,
