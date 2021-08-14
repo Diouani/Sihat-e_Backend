22c5dc13930e10933439dd6d37ec62c3e1efee0a
Diouani <73253036+Diouani@users.noreply.github.com>
2021-08-14 17:49:21 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -41,10 +41,10 @@ Route::middleware('auth:api')->group(function () {
 
 
 // height controller
-    Route::get('height',[HeightController::class , 'show']);
-    Route::post('height',[HeightController::class , 'create']);
+    Route::get('height/fetch',[HeightController::class , 'show']);
+    Route::post('height/create',[HeightController::class , 'create']);
     Route::get('height/{$id}',[HeightController::class , 'edit']);
-    Route::put('height/{$id}',[HeightController::class , 'update']);
+    Route::put('height/update/{$id}',[HeightController::class , 'update']);
 
 });
 
