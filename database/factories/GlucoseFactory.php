13f5f8bdd05ed2c361d2417c4b87e238ae488f0e
Diouani <73253036+Diouani@users.noreply.github.com>
2021-08-15 22:57:34 +0100

    update

---
+++ database/factories/GlucoseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Glucose;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GlucoseFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Glucose::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
